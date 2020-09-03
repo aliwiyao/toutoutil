6987734e6f3689b93e3a4f4d3a31c7210fc38ffc
admin <admin@ADMIN-PC>
2020-09-03 16:58:03 -0300

    Toutoutil One Released

---
+++ precache-manifest.45b6b64b25524eb16aed9c744836a263.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d5f757361e7d5b8976c5222d118313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e21ce7f949c727db5d",
+    "url": "/static/css/main.acf970f7.chunk.css"
+  },
+  {
+    "revision": "b6e85e6bc8a6ac9d3304",
+    "url": "/static/js/2.117d3ab6.chunk.js"
+  },
+  {
+    "revision": "63e21ce7f949c727db5d",
+    "url": "/static/js/main.67b32dd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
